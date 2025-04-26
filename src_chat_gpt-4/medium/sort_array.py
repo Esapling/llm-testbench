74ee9111176b60ff81139c750f39e1ddf1571ec9
Yunus Emre Bayraktar <122yeb122@gmail.com>
2025-04-26 13:35:47 +0300

    gpt4 - medium

---
+++ src_chat_gpt-4/medium/sort_array.py
@@ -0,0 +1,20 @@
+import unittest
+
+def sort_array(array):
+    """ Given an array of non-negative integers, return a sorted copy based on first and last value sum """
+    if not array:
+        return []
+    
+    if (array[0] + array[-1]) % 2 == 0:
+        return sorted(array, reverse=True)
+    return sorted(array)
+
+class Test(unittest.TestCase):
+    def test_sort_array(self):
+        self.assertEqual(sort_array([5]), [5])
+        self.assertEqual(sort_array([2, 4, 3, 0, 1, 5]), [0, 1, 2, 3, 4, 5])
+        self.assertEqual(sort_array([2, 4, 3, 0, 1, 5, 6]), [6, 5, 4, 3, 2, 1, 0])
+        self.assertEqual(sort_array([3, 1]), [1, 3])
+
+if __name__ == "__main__":
+    unittest.main()
