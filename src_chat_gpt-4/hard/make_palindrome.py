379ab5d31aba5cdb4225b70a5f078fd9f45d08a2
sapling <sirssapling@gmail.com>
2025-05-29 04:58:53 +0300

    Added new unit tests for make palindrome

--- src_chat_gpt-4/hard/make_palindrome.py
+++ src_chat_gpt-4/hard/make_palindrome.py
@@ -29,5 +29,44 @@ class Test(unittest.TestCase):
     def test_palindrome(self):
         self.assertEqual(make_palindrome('racecar'), 'racecar')
 
+    #### PHASE 2 Tests ####
+    def test_single_character(self):
+        self.assertEqual(make_palindrome('a'), 'a')
+        self.assertEqual(make_palindrome('l'), 'l')
+        self.assertEqual(make_palindrome(' '), ' ')
+
+    def test_already_palindrome(self):
+        self.assertEqual(make_palindrome('madam'), 'madam')
+        self.assertEqual(make_palindrome('level'), 'level')
+        self.assertEqual(make_palindrome('noon'), 'noon')
+    
+    def test_two_characters(self):
+        self.assertEqual(make_palindrome('ab'), 'aba')
+        self.assertEqual(make_palindrome('aa'), 'aa')
+        self.assertEqual(make_palindrome('zy'), 'zyz')
+
+    def test_strings_with_palindromic_suffixes(self):
+        self.assertEqual(make_palindrome('madama'), 'madamadam')
+        self.assertEqual(make_palindrome('rotavatorot'), 'rotavatorotavator')
+    def test_strings_with_no_palindromic_suffixes(self):
+        self.assertEqual(make_palindrome('hello'), 'hellolleh')
+        self.assertEqual(make_palindrome('world'), 'worldlrow')
+
+    def test_strings_with_special_characters(self):
+        self.assertEqual(make_palindrome('a!b!c'), 'a!b!c!b!a')
+        self.assertEqual(make_palindrome('race@car'), 'race@carac@ecar')
+
+    def test_case_sensitivity(self):
+        self.assertEqual(make_palindrome('Racecar'), 'RacecaracecaR')
+        self.assertEqual(make_palindrome('Level'), 'LeveleveL')
+
+    def test_long_strings(self):
+        
+        self.assertEqual(make_palindrome('abcdefgh'), 'abcdefghgfedcba')
+        self.assertEqual(make_palindrome('a' * 1000), 'a' * 1000)
+        long_palindrome ="saippuakivikauppias"
+        self.assertEqual(make_palindrome(long_palindrome[:-3]), long_palindrome)
+
+
 if __name__ == "__main__":
     unittest.main()
--- src_gemini_flash/hard/make_palindrome.py
+++ src_gemini_flash/hard/make_palindrome.py
@@ -4,6 +4,8 @@
 * Student IDs: 150200713, 150200020 
 */
 """
+import unittest
+
 def is_palindrome(string: str) -> bool:
     """ Test if given string is a palindrome """
     return string == string[::-1]
@@ -22,45 +24,44 @@ def make_palindrome(string: str) -> str:
     'catac'
     """
     if not string:
-        return ""
-    
-    # Find the longest palindromic suffix
-    for i in range(len(string), 0, -1):
-        if is_palindrome(string[:i]):
-            # Found the longest palindromic suffix
-            # Append the reverse of the remaining prefix
-            return string + string[i:][::-1]
-    
-    # If no palindromic suffix found (should not happen as the empty string is a palindrome)
-    return string + string[:-1][::-1]
+        return ''
 
+    beginning_of_suffix = 0
 
-# Unit tests
-import unittest
+    while not is_palindrome(string[beginning_of_suffix:]):
+        beginning_of_suffix += 1
+
+    return string + string[:beginning_of_suffix][::-1]
 
 class TestMakePalindrome(unittest.TestCase):
+
     def test_empty_string(self):
+        """Test with an empty input string."""
         self.assertEqual(make_palindrome(''), '')
-    
-    def test_single_char(self):
-        self.assertEqual(make_palindrome('a'), 'a')
-    
-    def test_already_palindrome(self):
-        self.assertEqual(make_palindrome('racecar'), 'racecar')
-        self.assertEqual(make_palindrome('level'), 'level')
-    
-    def test_example_cases(self):
+
+    def test_example_cat(self):
+        """Test with the example 'cat'."""
         self.assertEqual(make_palindrome('cat'), 'catac')
+
+    def test_example_cata(self):
+        """Test with the example 'cata'."""
         self.assertEqual(make_palindrome('cata'), 'catac')
-    
-    def test_palindromic_prefix(self):
-        self.assertEqual(make_palindrome('abc'), 'abcba')
-        self.assertEqual(make_palindrome('radar gun'), 'radar gunug radar')
-    
-    def test_partial_palindrome(self):
-        self.assertEqual(make_palindrome('abac'), 'abacaba')
-        self.assertEqual(make_palindrome('abba cat'), 'abba catac abba')
+
+    def test_already_palindrome(self):
+        """Test with a string that is already a palindrome."""
+        self.assertEqual(make_palindrome('racecar'), 'racecar')
+        self.assertEqual(make_palindrome('madam'), 'madam')
+
+    def test_complex_string(self):
+        """Test with a more complex string."""
+        self.assertEqual(make_palindrome('google'), 'googlelgoog')
+        self.assertEqual(make_palindrome('abab'), 'ababa')
+
     #### PHASE 2 Tests ####
+    def test_single_character(self):
+        self.assertEqual(make_palindrome('a'), 'a')
+        self.assertEqual(make_palindrome('l'), 'l')
+        self.assertEqual(make_palindrome(' '), ' ')
     
     def test_two_characters(self):
         self.assertEqual(make_palindrome('ab'), 'aba')
@@ -70,6 +71,9 @@ class TestMakePalindrome(unittest.TestCase):
     def test_strings_with_palindromic_suffixes(self):
         self.assertEqual(make_palindrome('madama'), 'madamadam')
         self.assertEqual(make_palindrome('rotavatorot'), 'rotavatorotavator')
+    def test_strings_with_no_palindromic_suffixes(self):
+        self.assertEqual(make_palindrome('hello'), 'hellolleh')
+        self.assertEqual(make_palindrome('world'), 'worldlrow')
 
     def test_strings_with_special_characters(self):
         self.assertEqual(make_palindrome('a!b!c'), 'a!b!c!b!a')
@@ -80,10 +84,13 @@ class TestMakePalindrome(unittest.TestCase):
         self.assertEqual(make_palindrome('Level'), 'LeveleveL')
 
     def test_long_strings(self):
+        
         self.assertEqual(make_palindrome('abcdefgh'), 'abcdefghgfedcba')
         self.assertEqual(make_palindrome('a' * 1000), 'a' * 1000)
         long_palindrome ="saippuakivikauppias"
         self.assertEqual(make_palindrome(long_palindrome[:-3]), long_palindrome)
 
-if __name__ == "__main__":
-    unittest.main()
+
+# This allows running the tests directly from the script
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
